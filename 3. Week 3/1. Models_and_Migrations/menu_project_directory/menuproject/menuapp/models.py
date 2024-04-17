dde9af40f8c9d9b1e9dc985914dbacf6c662d068
MUFARO CONEL NYAKUDYA <nyakudyamufa2002@gmail.com>
2024-04-17 08:14:06 +0200

    models and migrations

--- 3. Week 3/1. Models_and_Migrations/menu_project_directory/menuproject/menuapp/models.py
+++ 3. Week 3/1. Models_and_Migrations/menu_project_directory/menuproject/menuapp/models.py
@@ -5,8 +5,8 @@ from django.db import models
 # Create your models here.
 class Menu(models.Model):
     name = models.CharField(max_length=100)
-    cruisine = models.CharField(max_length=100) 
+    cuisine = models.CharField(max_length=100) 
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
 def _str_(self):
-    return self.name + ' : ' + self.cruisine + ' : ' + str(self.price)
+    return self.name + ' : ' + self.cuisine + ' : ' + str(self.price)
