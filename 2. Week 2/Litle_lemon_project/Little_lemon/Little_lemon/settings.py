0832c6d76eddb5c30527725de13cae148302c28a
MUFARO CONEL NYAKUDYA <nyakudyamufa2002@gmail.com>
2024-04-22 17:03:23 +0200

    removing mysql database

--- 2. Week 2/Litle_lemon_project/Little_lemon/Little_lemon/settings.py
+++ 2. Week 2/Litle_lemon_project/Little_lemon/Little_lemon/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    "myapp.apps.MyappConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -79,6 +80,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+
 
 
 # Password validation
--- 2. Week 2/Litle_lemon_project/Little_lemon/myapp/models.py
+++ 2. Week 2/Litle_lemon_project/Little_lemon/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    contact = models.CharField('Phone Number',max_length=100)
+    time = models.TimeField()
+    count =models.IntegerField
+    notes = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
